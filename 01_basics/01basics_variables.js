869c82eb0c398ebbabc1ec8723916f712fc0cf1a
vivek kumar <118981318+vivekcode01@users.noreply.github.com>
2024-09-05 11:27:28 +0000

    container config

---
+++ 01_basics/01basics_variables.js
@@ -0,0 +1,20 @@
+const accountId = 23132
+let accountEmail = "vivek989@gmail.com"
+var accountPassword ="12344"
+accountCity = "jaipur"
+let accountState;
+
+
+//accountId= 23//not allowed
+
+accountEmail ="viyv@vg.com"
+accountPassword = "779"
+accountCity = "bengaluru"
+
+console.log(accountId)
+/*Prefer not to use var 
+because of issue in block scope and functional scope
+
+
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
