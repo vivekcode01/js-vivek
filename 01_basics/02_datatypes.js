12bfa9cb58da7b597f3e497eda6f717e13ddb6c2
vivek kumar <118981318+vivekcode01@users.noreply.github.com>
2024-09-07 14:24:51 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" //treat all js code as newer version
+
+// alert(3+3) //we ae using node js,not browser
+
+console.log(3
+    +
+    3) // code readibility should be high
+
+    console.log("vivek")
+
+    let name ='vivek'
+    let age =19
+    let isLoggedIn= false
+
+
+    //number => 2 to power 53;
+    //bigint 
+    //string =>""
+    //boolean => true/false
+    //null => standalone
+    //undefiend =>
+    //symbol => unique
+
+
+    //objet
+
+    console.log(typeof undefined);
+    console.log(typeof null);
