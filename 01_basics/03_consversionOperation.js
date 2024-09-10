1b348da53d782d33b6e0e8662eb6a806bf3d8cfd
vivek kumar <118981318+vivekcode01@users.noreply.github.com>
2024-09-10 10:31:51 +0000

    conversion of datatype

---
+++ 01_basics/03_consversionOperation.js
@@ -0,0 +1,32 @@
+let score = "vivek"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//"33"=> 33
+//"33abx"=> Nan (not a number)
+// true=>1  false=> 0
+
+
+let isLoggedIn ="vivek"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//0=> false, 1=> true
+//"" => false  , 
+// "vivek" => true
+
+let someNumber = 23
+let stringNumber = String(someNumber)
+
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
