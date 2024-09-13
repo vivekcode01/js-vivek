c73fd949f53c96a49a1eda5cc3e83edb6f078b27
vivek kumar <118981318+vivekcode01@users.noreply.github.com>
2024-09-13 05:52:02 +0000

    conversion of datatype

--- 01_basics/03_consversionOperation.js
+++ 01_basics/03_consversionOperation.js
@@ -30,3 +30,45 @@ let stringNumber = String(someNumber)
 
 
 
+//****************************operation**************************//
+
+let  value = 3;
+let negValue =-value;
+//console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1= "vivek"
+let str2 =" kumar"
+
+let str3 = str1 +str2;
+//console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+ "2")
+// console.log("1"+2+3)
+// console.log(1+2+"3")
+
+
+// console.log((3+4)*4/6)
+
+
+// console.log(+true)
+// console.log(+"")
+
+
+let num1,num2,num3
+
+num1=num2=num3=4+3
+
+let gamecounter =100
+++gamecounter;
+console.log(gamecounter);
+
+//link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
