ba223c2c21fb1becfaf019c293e33853242ee9bf
vivek kumar <118981318+vivekcode01@users.noreply.github.com>
2024-09-15 13:35:49 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,12 @@
+const name = "vivek"
+const repoCount = 40
+
+//console.log(name + repoCount + " Value")
+
+console.log(`hello my name is ${name} and my repocount is ${repoCount}`)
+
+const gamename = new String('hitesh-hc-com')
+
+console.log(gamename[0]);
+
+console.log(gamename.__proto__);
