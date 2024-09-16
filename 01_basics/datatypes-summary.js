b731451e61e49b6c3a8dbc0fecec945c296704da
vivek kumar <118981318+vivekcode01@users.noreply.github.com>
2024-09-16 07:49:16 +0000

    datatype summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -34,3 +34,27 @@ console.log(typeof anotherId)
 
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//*************************************************************** */
+
+//stack(primitive)  heap(non primitive)
+
+let myname = "vivek kumar"
+ 
+let anothername= myname;
+anothername = "rohit";
+
+console.log(myname)
+console.log(anothername)
+
+let userOne= {
+    email: "vivek@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email ="vivek2892@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
