66affc2cc7f688b1622cab26c8afc2194cf31f42
vivek kumar <118981318+vivekcode01@users.noreply.github.com>
2024-09-16 06:32:09 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+//primitive
+
+// 7 type: string ,number,null,undefined,boolean,symbol,bigint
+
+const score =100
+const scorevalue= 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail ;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id===anotherId)
+
+// const bigNumber = 2452614816255861542n
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const hero =["vivek","sakti","doga"]
+let myObj ={
+    name :"vivek",
+    age : 23,
+}
+
+const myFunction =function(){
+    console.log("hello world")
+}
+
+console.log(typeof anotherId)
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
