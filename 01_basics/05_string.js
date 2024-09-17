3d11da3835c94abbbeaa8067e659d9d70b21da3b
vivek kumar <118981318+vivekcode01@users.noreply.github.com>
2024-09-17 13:42:37 +0000

    string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -5,8 +5,33 @@ const repoCount = 40
 
 console.log(`hello my name is ${name} and my repocount is ${repoCount}`)
 
-const gamename = new String('hitesh-hc-com')
+const gameName = new String('vivek-kumar-123-com')
 
-console.log(gamename[0]);
+// console.log(gameName[1])
+// console.log(gameName.__proto__)
 
-console.log(gamename.__proto__);
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('v'))
+
+
+const newstring = gameName.substring(0,4)
+console.log(newstring)
+
+const anotherstring = gameName.slice(-8,4)
+console.log(anotherstring)
+
+
+const newstringone = "  vivek  kumar    ";
+console.log(newstringone)
+console.log(newstringone.trim())
+
+
+const url = "https://vivek.com/vivek20%kumar"
+
+console.log(url.replace('20%' , '=---'))
+
+console.log(url.includes('vivekf'))
+
+console.log(gameName.split('-'))
