5e01cf2a9827c4d8d7996b72f6e2d637dec62c67
vivek kumar <118981318+vivekcode01@users.noreply.github.com>
2024-10-07 19:53:37 +0000

    scope this and arrow

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,61 @@
+var c =300;
+let a =200;
+
+if(true){
+    let a= 10;
+    const b =309
+    //console.log("ineer:", a)
+}
+
+//console.log(a)
+//console.log(b)
+//console.log(c)
+
+
+
+
+
+function one (){
+    const username = "vivek"
+
+
+    function two() {
+        const website ="youtube";
+        console.log(username);
+        
+    }
+
+    //console.log(website);
+    two()
+    
+    
+}
+//one()
+
+
+if(true){
+    const username ="vivek"
+    if(username ==="vivek"){
+        const website = "youtube"
+      //  console.log(username +website)
+    }
+    //console.log(website)
+}
+
+//console.log(username)
+
+
+
+
+//+++++++++++++++++++++++++++++interesting+++++++++++++++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+    return  num+1;
+}
+
+console.log(addtwo(3))      // if we shift this line two 62 it will work
+
+const addtwo = function(num){
+    return num +2;
+}
