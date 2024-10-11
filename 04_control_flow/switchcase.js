e678614be5dc592576f6a13766d23c52d42e61de
vivek kumar <118981318+vivekcode01@users.noreply.github.com>
2024-10-11 12:18:24 +0000

    switch

---
+++ 04_control_flow/switchcase.js
@@ -0,0 +1,57 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month =4;
+
+// switch(month){
+//     case 1:
+//     console.log("january")
+//     break;
+//     case 2:
+//         console.log("February")
+//         break;
+//     case 3:
+//         console.log("march")
+//         break;
+//     case 4:
+//         console.log("april")
+//         break;
+
+//     default:
+//         console.log("default")
+//         break
+
+// }
+
+
+const month ="march";
+
+switch(month){
+    case "jan":
+    console.log("january")
+    break;
+    case "feb":
+        console.log("February")
+        break;
+    case "march":
+        console.log("march")
+        break;
+    case "april":
+        console.log("april")
+        break;
+
+    default:
+        console.log("default")
+        break
+
+}
+
+
+
