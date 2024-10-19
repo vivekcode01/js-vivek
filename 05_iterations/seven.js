f785f35883ff07898f9ecb0322e8dbbe19fd7daa
vivek kumar <118981318+vivekcode01@users.noreply.github.com>
2024-10-19 18:38:44 +0000

    chaining

---
+++ 05_iterations/seven.js
@@ -0,0 +1,15 @@
+const myNumbers =[ 1,2,3,4,5,6,7,8,9,10]
+
+//const newnums =myNumbers.map((nums)=> nums = nums+10)
+
+// const newnums =myNumbers
+//                         .map((num) => num = num*10)
+//                         .map((num)=> num = num +1)
+
+
+const newnums =myNumbers
+                        .map((num) => num = num*10)
+                        .map((num)=> num = num +1)
+                        .filter((num)=> num>= 40)
+
+console.log(newnums)
